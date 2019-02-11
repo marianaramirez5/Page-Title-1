940a4589036a12acf5be257ac21a4039b3870a3d
marianaramirez5 <44061354+marianaramirez5@users.noreply.github.com>
2019-02-11 10:24:06 -0500

    Created using Popcode: https://popcode.org

--- script.js
+++ script.js
@@ -3,5 +3,13 @@ $(".apples").click(function() {
     $(".apples").css("display", "none");
     $(".hi").hide();
     $(".bottle").show();
+   
+    
 });
 
+$(".potato").click(function(){
+    $(".bottle").css("display","block");
+    $(".potato").css("display","none");
+    $(".bottle").hide();
+     $(".done").show();
+});
